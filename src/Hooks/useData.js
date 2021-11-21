392808494516adaf0f26fc1cbde9d3c877e3b75e
Sumonbhuiya <sumonbhuiya97@gmail.com>
2021-11-21 23:14:26 +0600

    data link

--- src/Hooks/useData.js
+++ src/Hooks/useData.js
@@ -5,7 +5,7 @@ const useData = () => {
 
     useEffect(() => {
         // load data for services
-        fetch(`https://sumonbhuiya.github.io/helthcare-data/doctors.JSON`)
+        fetch(`https://sumonbhuiya.github.io/json-all-data/doctors.JSON`)
             .then(res => res.json())
             .then(data => setService(data));
     }, []);
